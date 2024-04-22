725249c04633990ac8b5df86db413605fcd71c05
RadounaeBaba <radoine.baba04@gmail.com>
2024-04-22 17:54:25 +0000

    asyncio

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" async corouting delay """
+import random
+import asyncio
+
+
+async def wait_random(max_delay=10):
+    """ wait random time """
+    num = random.uniform(0, max_delay)
+    await asyncio.sleep(num)
+    return num
