b93a392169acba80612e3f94e25405c83df87e25
RadounaeBaba <radoine.baba04@gmail.com>
2024-04-23 17:26:12 +0000

    fix0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,8 +2,10 @@
 """ async_generator implementation """
 import asyncio
 import random
+from typing import Generator
 
-async def async_generator():
+
+async def async_generator() -> Generator[float, None, None]:
     """ yield a random number """
     for i in range(10):
         await asyncio.sleep(1)
